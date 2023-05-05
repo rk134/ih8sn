a8da2ff4ca1ecede10803fb090abbf5ea83408c7
althafvly <althafvly@gmail.com>
2023-05-06 01:02:01 +0530

    ih8sn: add support for all os builds

--- product.mk
+++ product.mk
@@ -4,12 +4,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/etc/ih8sn.conf \
     system/etc/init/ih8sn.rc
 
-PRODUCT_PACKAGES += ih8sn
-
-ifneq ("$(wildcard ih8sn/system/etc/ih8sn.conf.$(subst lineage_,,$(TARGET_PRODUCT)))","")
-PRODUCT_COPY_FILES += \
-    ih8sn/system/etc/ih8sn.conf.$(subst lineage_,,$(TARGET_PRODUCT)):$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-else
-PRODUCT_COPY_FILES += \
-    ih8sn/system/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-endif
+PRODUCT_PACKAGES += \
+    ih8sn \
+    ih8sn.conf
