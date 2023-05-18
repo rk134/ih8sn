17fd6dd66169931c86a774f18cfc7ccc1018d1f8
althafvly <althafvly@gmail.com>
2023-05-18 19:42:00 +0530

    ih8sn: copy config properly

--- product.mk
+++ product.mk
@@ -4,6 +4,12 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/etc/ih8sn.conf \
     system/etc/init/ih8sn.rc
 
-PRODUCT_PACKAGES += \
-    ih8sn \
-    ih8sn.conf
+PRODUCT_PACKAGES += ih8sn
+
+ifneq ("$(wildcard ih8sn/system/etc/ih8sn.conf.$(shell echo $(TARGET_PRODUCT) | cut -d'_' -f2-))","")
+PRODUCT_COPY_FILES += \
+    ih8sn/system/etc/ih8sn.conf.$(shell echo $(TARGET_PRODUCT) | cut -d'_' -f2-):$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+else
+PRODUCT_COPY_FILES += \
+    ih8sn/system/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+endif
