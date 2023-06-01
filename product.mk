2c314311e2759ffb48e080621dbbc706ef9830e0
althafvly <althafvly@gmail.com>
2023-06-01 20:26:25 +0530

    ih8sn: product: Bit of improvements

--- product.mk
+++ product.mk
@@ -6,9 +6,11 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 
 PRODUCT_PACKAGES += ih8sn
 
-ifneq ("$(wildcard ih8sn/system/etc/ih8sn.conf.$(shell echo $(TARGET_PRODUCT) | cut -d'_' -f2-))","")
+PRODUCT_CONFIGURATION_FILE := ih8sn.conf.$(shell echo $(TARGET_PRODUCT) | cut -d'_' -f2-)
+
+ifneq ("$(wildcard ih8sn/system/etc/$(PRODUCT_CONFIGURATION_FILE))","")
 PRODUCT_COPY_FILES += \
-    ih8sn/system/etc/ih8sn.conf.$(shell echo $(TARGET_PRODUCT) | cut -d'_' -f2-):$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+    ih8sn/system/etc/$(PRODUCT_CONFIGURATION_FILE):$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
 else
 PRODUCT_COPY_FILES += \
     ih8sn/system/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
